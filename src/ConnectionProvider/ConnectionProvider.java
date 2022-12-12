a348e81ebe451edfffeccc1557ad1055b97dcb8e
Bharath <vaddibharath2000@gmail.com>
2022-12-12 09:38:52 +0530

    adding here

---
+++ src/ConnectionProvider/ConnectionProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConnectionProvider;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ConnectionProvider {
+    //public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentresultssystem", "root", "Bharath@1439" );
+            return con;
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+            return null;
+        }
+    }
+//    public static void main(String args[]){
+//        ConnectionProvider.getCon();
+//    }
+}
+
+
+//}
